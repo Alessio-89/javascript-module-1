566a9fd0355cc9e08affbd5de2b0610acf0b003e
Alessio-89 <133093821+Alessio-89@users.noreply.github.com>
2023-06-15 11:04:03 +0200

    Add files via upload

---
+++ week-1/InClass/exercise-B.js
@@ -0,0 +1,10 @@
+Halo, dunia! // Indonesian
+Ciao, mondo! // Italian
+Hola, mundo! // Spanish
+Hola, món! // Catalan
+Saludi, mundu! // Sardinian
+Bonjour, monde! //French
+Hola, mundo! // Portuguese
+Hallo, Welt! // German
+Hallo Wereld! // Dutch
+Привет, мир //Russian
