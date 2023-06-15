100b800a02b48bb64f92e4790ae5075a5ccf6ae2
Alessio-89 <133093821+Alessio-89@users.noreply.github.com>
2023-06-15 10:10:31 +0200

    Add files via upload

---
+++ week-1/InClass/exercise-E.js
@@ -0,0 +1,8 @@
+//Write a program that logs a message with a greeting and your name using the two concatenation methods we used
+
+const greeting = "Hi";
+const myName = "Alessio";
+
+const fullGreeting = `${greeting}, my name is ${myName}!`;
+
+console.log(fullGreeting); // Logs "Hi, my name is Alessio!"
