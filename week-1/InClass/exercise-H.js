ecb2980e1bdc45faf3e0dd7082809690502700d0
Alessio-89 <133093821+Alessio-89@users.noreply.github.com>
2023-06-20 18:29:34 +0200

    Add files via upload

---
+++ week-1/InClass/exercise-H.js
@@ -0,0 +1,16 @@
+const functionConcatenate = (string1, string2, number) => {
+
+//this simple function takes two strings and a number as parameters.
+
+result = number*2;
+
+    return "the concatenated string is " + string1 + string2 + result.toString();
+
+    //concatenates the two strings, plus the number (converted to string with the toString() method)
+}
+
+console.log(functionConcatenate("a", "b", 5)); //prints "the concatenated string is ab10"
+
+
+
+
